39648f3c393b3698b15f058608ec7d0b2e2477dc
Thoams White <tsw96d@umsystem.edu>
2025-10-20 13:20:41 -0500

    updated example

--- example.py
+++ example.py
@@ -1,22 +1,24 @@
 from dlc_clustering.projects import Project
-from dlc_clustering.clustering import PCAKMeansBoutStrategy
-from dlc_clustering.data_processing import CentroidDiffStrategy, VelocityStrategy, VelocityFilterStrategy, PairwiseDistanceStrategy, SpectrogramBoutStrategy
-import matplotlib.pyplot as plt
-import polars as pl
+from dlc_clustering.clustering import UnsupervisedBoutPipeline, SKStandardScaler, SKPCA, SKKMeans
+from dlc_clustering.data_processing import CentroidDiffStrategy, VelocityStrategy, VelocityFilterStrategy, ConfidenceFilterStrategy
+from dlc_clustering.data_processing import AnglesStrategy
 from dlc_clustering.graphing import graph_all
 from dlc_clustering.video_processing import render_cluster_videos
 
+
 # Define the data processing strategies and clustering strategy
 data_processing_strategies = []
+data_processing_strategies.append(AnglesStrategy([["nose", "tail_base", "tail_end"]]))
 data_processing_strategies.append(CentroidDiffStrategy())
 data_processing_strategies.append(VelocityStrategy())
 data_processing_strategies.append(VelocityFilterStrategy(percentile_threshold=0.75))
-data_processing_strategies.append(SpectrogramBoutStrategy(["nose"], stride=5))
-data_processing_strategies.append(PairwiseDistanceStrategy([["nose", "tail_base"],
-                 ["tail_base", "tail_end"],
-                 ["left_earbase", "right_earbase"]]))
+data_processing_strategies.append(ConfidenceFilterStrategy(percentile_threshold=0.75))
 
-clustering_strategy=PCAKMeansBoutStrategy(stride=5)
+clustering_strategy = UnsupervisedBoutPipeline(
+    preprocessors=[SKStandardScaler(), SKPCA(n_components=10)],
+    clusterer=SKKMeans(n_clusters=8, random_state=42),
+    bout_length=15, stride=5
+)
 
 project_path = "/home/thomas/Documents/dlc_clustering/data/example_project"
 # The files in the project directory should be in the format:
